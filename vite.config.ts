67e723edabedb7627bd6a22283e934974c408a87
benyaminha <benyamin.ha03@gmail.com>
2026-03-13 18:08:31 -0700

    updated build base

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
+    base: '/Stephify-Wrapped/'
   ],
   resolve: {
     alias: {
